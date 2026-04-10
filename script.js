8960204a1e2fb57d89f0f824b671bc9ce710caec
Jufi Rachmana Dwi Chandra <triutami0187@gmail.com>
2026-04-10 19:53:07 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,45 @@
+//* ==== Informasi Pribadi / Profil Pribadi ==== *//
+
+const hardSkills = [
+  "Basic English Speaking, Listening And Reading",
+  "Operating Microsoft Office ( Microsoft Word, Excel And Powerpoint",
+  "Operating Html, Css And JavaScript Language",
+];
+
+const softSkills = [
+  "Working In Pressure",
+  "Communication",
+  "Serve Customer",
+  "Team Work",
+  "Problem Solving",
+];
+
+const text =
+  "my specific measurable technical skills" +
+  hardSkills +
+  " " +
+  "This skill is important for my career because it improves collaboration, leadership, and adaptation" +
+  softSkills;
+
+// make object
+// object literal
+// this
+
+const Biodata = {
+  name: "Jufi Rachmana D.C.",
+  age: "19 Years",
+  school: "State Senior High School 8 Garut",
+  major: "Natural Science/ IPA",
+  grade: "Grade 87,08",
+  Email: "jufirachmanadwichandra@gmail.com",
+  PhoneNumber: "+62895-4174-06600",
+};
+
+// Global
+// Dom Selection
+console.log(document.title);
+console.log(document.body);
+
+// document.querySelectorALL()
+const p = document.querySelectorAll("p");
+const div = document.querySelectorAll("div");
